0b5f83e068a74a4f12d82712857fe7ce88f3e7a9
Patrick Kelly <patrick.kelly@sherwin.com>
2016-06-16 15:29:00 -0400

    WIP

--- PasswordSystem/PasswordVerifier.cs
+++ PasswordSystem/PasswordVerifier.cs
@@ -1,7 +1,12 @@
-﻿namespace PasswordSystem
+﻿using System;
+
+namespace PasswordSystem
 {
     public class PasswordVerifier
     {
-        
+        public void Verify(string password)
+        {
+            if (string.IsNullOrEmpty(password)) ;
+        }
     }
 }
--- PasswordSystemTests/PasswordVerifierTests.cs
+++ PasswordSystemTests/PasswordVerifierTests.cs
@@ -1,9 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using PasswordSystem;
 
 namespace PasswordSystemTests
 {
     [TestFixture]
     public class PasswordVerifierTests
     {
+        private PasswordVerifier _passwordVerifier;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _passwordVerifier = new PasswordVerifier();
+        }
+
+        [Test]
+        public void Password_Cannot_Be_Null()
+        {
+            var passwordVerifier = new PasswordVerifier();
+
+            Assert.Throws<ArgumentNullException>(() => passwordVerifier.Verify(null));
+        }
+
+        [Test]
+        public void NotNullPassword_DoesNotThrow()
+        {
+            var password = "abc";
+            Assert.DoesNotThrow(()=>_passwordVerifier.Verify(password));
+        }
+
+        [Test]
+        public void Password_Is_Atleast_Eight_Characters()
+        {
+            var password = "1234567";
+            Assert.Throws<ArgumentException>(() => _passwordVerifier.Verify(password));
+
+        }
     }
 }
